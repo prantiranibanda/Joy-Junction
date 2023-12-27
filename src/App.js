d6d0225f272093e478936dbb333bb589875c9a11
pranti <prantibanda@gmail.com>
2023-12-28 00:07:08 +0530

    Navbar Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
-import './components/Navbar';
 import './App.css';
 import Navbar from './components/Navbar';
+import Greet from './components/Greet';
 
 function App() {
   return (
     <div className="text">
       <Navbar/>
+      <Greet/>
     </div>
   );
 }
---
+++ src/components/Greet.js
@@ -0,0 +1,8 @@
+function Greet(){
+    return(
+        <div className="p-32 font-mono font-bold text-center text-5xl text-cyan-200">
+            Welcome to the Gaming World!!
+        </div>
+    );
+}
+export default Greet;
