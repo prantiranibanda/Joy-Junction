d68764c339b39af75f3926c89ca5c0e0c43bfe18
pranti <prantibanda@gmail.com>
2023-12-28 11:41:42 +0530

    App.js is changed now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import Greet from './components/Greet';
-import Button from './components/Button';
+import Home from './components/Home';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,19 +8,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navbar/>}>
-
+          <Route path="home" element={<Home/>}/>
         </Route>
-          <Greet/>
-          <div className="flex justify-center space-x-96 pt-8">
-            <div className="flex flex-col justify-between w-80">
-              <img src="/tic-tac-toe.webp" alt="tictactoe" className="w-80 h-72"/>
-              <Button button="Tic-Tac-Toe"/>
-            </div>
-            <div className="flex flex-col justify-between w-80">
-              <img src="/diceghema.png" alt="dice" className="w-80 h-62 my-auto"/>
-              <Button button="Dice-Rolling"/>
-            </div>
-          </div>
       </Routes>
     </BrowserRouter>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,17 @@
+import { Outlet, Link } from "react-router-dom";
 function Navbar(){
     return(
         <>
         <div className="bg-orange-400 flex items-center py-3 border-b-8 border-yellow-300">
             <div className="mx-10 font-bold text-3xl text-blue-950 italic">Play</div>
             <div className="flex space-x-10 ml-auto mr-10 text-white text-xl font-bold box-border">
-                <div className="px-3 py-2 rounded-lg border-2 border-red-500 bg-orange-500 hover:border-white hover:bg-yellow-300 hover:text-pink-500 hover:cursor-pointer">Home</div>
+                <Link to="/"><div className="px-3 py-2 rounded-lg border-2 border-red-500 bg-orange-500 hover:border-white hover:bg-yellow-300 hover:text-pink-500 hover:cursor-pointer">Home</div></Link>
                 <div className="px-3 py-2 rounded-lg border-2 border-red-500 bg-orange-500 hover:border-white hover:bg-yellow-300 hover:text-pink-500 hover:cursor-pointer">Game1</div>
                 <div className="px-3 py-2 rounded-lg border-2 border-red-500 bg-orange-500 hover:border-white hover:bg-yellow-300 hover:text-pink-500 hover:cursor-pointer">Game2</div>
             </div>
         </div>
         <div className="border-[#fa02e1] border-b-4"></div>
+        <Outlet/>
         </>
     );
 }
