3076056525859a28e1858722a0ce252ba0ba4f4d
pranti <prantibanda@gmail.com>
2023-12-28 15:48:04 +0530

    Board ready

--- src/components/Game1.js
+++ src/components/Game1.js
@@ -2,10 +2,11 @@ import { useState } from "react";
 
 let flag = 0;
 function Game1(){
-    
     return(
         <div className="flex justify-center items-center h-[86vh]">
-            <div className="grid grid-cols-3 gap-1 w-60 h-60 bg-orange-400">
+            <div className="border-8 border-[#fa02e1]">
+            <div className="border-4 border-red-600">
+            <div className="grid grid-cols-3 gap-2 w-64 h-64 bg-orange-400 border-8 border-yellow-300">
                 <MyButton/>
                 <MyButton/>
                 <MyButton/>
@@ -16,13 +17,15 @@ function Game1(){
                 <MyButton/>
                 <MyButton/>
             </div>
+            </div>
+            </div>
         </div>
     );
 }
 function MyButton() {
     const [isZero, setIsZero] = useState(true);
     let done = false;
-    const [str, setStr] = useState("");
+    const [str, setStr] = useState(".");
     function handleClick(){
         setIsZero((isZero)?false:true);
         if(flag === 0 && done === false){
@@ -37,9 +40,15 @@ function MyButton() {
         }
         
     }
-    
+    let colo = "text-orange-500";
+    if(str === "o"){
+        colo = "text-white";
+    }
+    else if(str === "x"){
+        colo = "text-[#2206c2]";
+    }
     return (
-        <div className="bg-orange-500" onClick={handleClick}>{str}</div>
+        <div className={`bg-orange-500 flex justify-center items-baseline text-6xl font-medium ${colo}`} onClick={handleClick}>{str}</div>
     );
 }
 export default Game1;
