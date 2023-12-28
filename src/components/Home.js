83e5c6985af286bfc6087b8bd0f82c17b66d1240
pranti <prantibanda@gmail.com>
2023-12-28 12:11:38 +0530

    Extra pictures deleted

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,11 @@
 import Greet from "./Greet";
-import Button from "./Button";
+import Gameoption from "./Gameoption";
 
 function Home(){
     return(
         <>
             <Greet/>
-            <div className="flex justify-center space-x-96 pt-8">
-                <div className="flex flex-col justify-between w-80">
-                <img src="/tic-tac-toe.webp" alt="tictactoe" className="w-80 h-72"/>
-                <Button button="Tic-Tac-Toe"/>
-                </div>
-                <div className="flex flex-col justify-between w-80">
-                <img src="/diceghema.png" alt="dice" className="w-80 h-62 my-auto"/>
-                <Button button="Dice-Rolling"/>
-                </div>
-            </div>
+            <Gameoption/>
         </>
     );
 }
