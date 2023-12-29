edf8d2de91d3458add2aaacda24e50cb83d810a2
pranti <prantibanda@gmail.com>
2023-12-29 09:54:56 +0530

    If are made else if

--- src/components/Game1.js
+++ src/components/Game1.js
@@ -20,60 +20,60 @@ function Game1(){
         if((arr[1] === arr[2]) && (arr[2] === arr[3]) && (arr[3] === "x")){
             setDisplayWin(<Winner func={func} win="Winner: playerX"/>);
         }
-        if((arr[4] === arr[5]) && (arr[5] === arr[6]) && (arr[6] === "x")){
+        else if((arr[4] === arr[5]) && (arr[5] === arr[6]) && (arr[6] === "x")){
             setDisplayWin(<Winner func={func} win="Winner: playerX"/>);
         }
-        if((arr[7] === arr[8]) && (arr[8] === arr[9]) && (arr[9] === "x")){
+        else if((arr[7] === arr[8]) && (arr[8] === arr[9]) && (arr[9] === "x")){
             setDisplayWin(<Winner func={func} win="Winner: playerX"/>);
         }
         //col x
-        if((arr[1] === arr[4]) && (arr[4] === arr[7]) && (arr[7] === "x")){
+        else if((arr[1] === arr[4]) && (arr[4] === arr[7]) && (arr[7] === "x")){
             setDisplayWin(<Winner func={func} win="Winner: playerX"/>);
         }
 
-        if((arr[2] === arr[5]) && (arr[5] === arr[8]) && (arr[8] === "x")){
+        else if((arr[2] === arr[5]) && (arr[5] === arr[8]) && (arr[8] === "x")){
             setDisplayWin(<Winner func={func} win="Winner: playerX"/>);
         }
-        if((arr[3] === arr[6]) && (arr[6] === arr[9]) && (arr[9] === "x")){
+        else if((arr[3] === arr[6]) && (arr[6] === arr[9]) && (arr[9] === "x")){
             setDisplayWin(<Winner func={func} win="Winner: playerX"/>);
         }
         //diagonal x
-        if((arr[1] === arr[5]) && (arr[5] === arr[9]) && (arr[9] === "x")){
+        else if((arr[1] === arr[5]) && (arr[5] === arr[9]) && (arr[9] === "x")){
             setDisplayWin(<Winner func={func} win="Winner: playerX"/>);
         }
-        if((arr[3] === arr[5]) && (arr[5] === arr[7]) && (arr[7] === "x")){
+        else if((arr[3] === arr[5]) && (arr[5] === arr[7]) && (arr[7] === "x")){
             setDisplayWin(<Winner func={func} win="Winner: playerX"/>);
         }
 
         //row o
-        if((arr[1] === arr[2]) && (arr[2] === arr[3]) && (arr[3] === "o")){
+        else if((arr[1] === arr[2]) && (arr[2] === arr[3]) && (arr[3] === "o")){
             setDisplayWin(<Winner func={func} win="Winner: playerO"/>);
         }
-        if((arr[4] === arr[5]) && (arr[5] === arr[6]) && (arr[6] === "o")){
+        else if((arr[4] === arr[5]) && (arr[5] === arr[6]) && (arr[6] === "o")){
             setDisplayWin(<Winner func={func} win="Winner: playerO"/>);
         }
-        if((arr[7] === arr[8]) && (arr[8] === arr[9]) && (arr[9] === "o")){
+        else if((arr[7] === arr[8]) && (arr[8] === arr[9]) && (arr[9] === "o")){
             setDisplayWin(<Winner func={func} win="Winner: playerO"/>);
         }
         //col o
-        if((arr[1] === arr[4]) && (arr[4] === arr[7]) && (arr[7] === "o")){
+        else if((arr[1] === arr[4]) && (arr[4] === arr[7]) && (arr[7] === "o")){
             setDisplayWin(<Winner func={func} win="Winner: playerO"/>);
         }
 
-        if((arr[2] === arr[5]) && (arr[5] === arr[8]) && (arr[8] === "o")){
+        else if((arr[2] === arr[5]) && (arr[5] === arr[8]) && (arr[8] === "o")){
             setDisplayWin(<Winner func={func} win="Winner: playerO"/>);
         }
-        if((arr[3] === arr[6]) && (arr[6] === arr[9]) && (arr[9] === "o")){
+        else if((arr[3] === arr[6]) && (arr[6] === arr[9]) && (arr[9] === "o")){
             setDisplayWin(<Winner func={func} win="Winner: playerO"/>);
         }
         //diagonal o
-        if((arr[1] === arr[5]) && (arr[5] === arr[9]) && (arr[9] === "o")){
+        else if((arr[1] === arr[5]) && (arr[5] === arr[9]) && (arr[9] === "o")){
             setDisplayWin(<Winner func={func} win="Winner: playerO"/>);
         }
-        if((arr[3] === arr[5]) && (arr[5] === arr[7]) && (arr[7] === "o")){
+        else if((arr[3] === arr[5]) && (arr[5] === arr[7]) && (arr[7] === "o")){
             setDisplayWin(<Winner func={func} win="Winner: playerO"/>);
         }
-        if(arr[1]!==1 && arr[2]!==2 && arr[3]!==3 && arr[4]!==4 && arr[5]!==5 && arr[6]!==6 && arr[7]!==7 && arr[8]!==8 && arr[9]!==9){
+        else if(arr[1]!==1 && arr[2]!==2 && arr[3]!==3 && arr[4]!==4 && arr[5]!==5 && arr[6]!==6 && arr[7]!==7 && arr[8]!==8 && arr[9]!==9){
             setDisplayWin(<Draw/>);
         }
     }
@@ -118,7 +118,9 @@ function MyButton({no, handleWin, isZero, setIsZero}){
             }
             setDone(true);
         }
-        handleWin();//setTimeout
+        setTimeout(()=>{
+            handleWin();
+        },200)
     }
     // let colo = "text-orange-500";
     // if(str === "o"){
