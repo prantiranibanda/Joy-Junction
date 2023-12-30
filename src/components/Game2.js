f65ee1d2954fc57237ccc61a276068eba73778e5
pranti <prantibanda@gmail.com>
2023-12-30 12:31:28 +0530

    Dice image added

--- src/components/Game2.js
+++ src/components/Game2.js
@@ -1,15 +1,17 @@
 import { useState } from "react";
 
 function Game2(){
-    const [randomNumber, setRandomNumber] = useState(null);
+    const [randomDiceImg, setRandomDiceImg] = useState(null);
+
     function rollDice(){
-        setRandomNumber((Math.floor(Math.random() * 6))+1);
+        let randomNumber = ((Math.floor(Math.random() * 6))+1);
+        setRandomDiceImg(<img src={`/images/ds${randomNumber}.jpg`} alt={`dice${randomNumber}`}></img>)
     }
     
     return(
         <div className="flex flex-col justify-center items-center h-[80vh]">
-            <div className="bg-green-700 w-56 h-56 rounded-lg text-white font-bold text-2xl">{randomNumber}</div>
-            <button className="bg-yellow-300 text-pink-700 px-6 py-3 rounded" onClick={rollDice}>Click to roll</button> 
+            <div className="w-56 h-56 rounded-lg text-white font-bold text-2xl">{randomDiceImg}</div>
+            <button className="bg-yellow-300 text-pink-700 px-6 py-3 rounded mt-5" onClick={rollDice}>Click to roll</button> 
         </div>
         
     );
