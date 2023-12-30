2be9fdd300958843ae4800ab1f1d89bc885b325d
pranti <prantibanda@gmail.com>
2023-12-30 12:00:48 +0530

    First step of dice rolling done

--- src/components/Game2.js
+++ src/components/Game2.js
@@ -1,6 +1,17 @@
+import { useState } from "react";
+
 function Game2(){
+    const [randomNumber, setRandomNumber] = useState(null);
+    function rollDice(){
+        setRandomNumber((Math.floor(Math.random() * 6))+1);
+    }
+    
     return(
-        <div>Game2</div>
+        <div className="flex flex-col justify-center items-center h-[80vh]">
+            <div className="bg-green-700 w-56 h-56 rounded-lg text-white font-bold text-2xl">{randomNumber}</div>
+            <button className="bg-yellow-300 text-pink-700 px-6 py-3 rounded" onClick={rollDice}>Click to roll</button> 
+        </div>
+        
     );
 }
 export default Game2;
