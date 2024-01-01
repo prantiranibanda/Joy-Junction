242ba124cf64db5753a7828c463cefa9f212cec4
pranti <prantibanda@gmail.com>
2024-01-01 16:27:32 +0530

    Sound added and buttonArray is converted to buttonArray1

--- src/components/Game2.js
+++ src/components/Game2.js
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
+import win from '../audio/win.wav';
+import click from '../audio/click.mp3';
+
 let f = false;
+let wino = new Audio(win);
 function Game2(){
     const [randomDiceImg, setRandomDiceImg] = useState(<img src="/dice.png" alt="dice" ></img>);
     const [sum, setSum] = useState(0);
     const [randomNumber, setRandomNumber] = useState(1);
     const [flag, setFlag] = useState(false);
     const [isWin, setIsWIn] = useState(false);
+
+    //array creation
     let arr = new Array();
     let k=111;
     for(let i=0; i<10; i++){
@@ -34,32 +40,46 @@ function Game2(){
             }
         }
     }
-    //console.log(arr);
-    const [buttonArray, setButtonArray] = useState(arr);
+    const [buttonArray1, setButtonArray1] = useState(arr);
     let temp;
+
+
+    //Sound effects.......................
+    function winclick(){
+        wino = new Audio(win);
+        return wino.play()
+    }
+    function playy(){
+        return new Audio(click).play()
+    }
+    //.....................................
+
     useEffect(()=>{
-        setButtonArray(
+        setButtonArray1(
             (prev)=>{
                 temp = prev.map((eachbutton)=>
-                    (eachbutton.id === sum)?{...eachbutton, data: "rounded-full bg-green-400", isEqualToSum: true}:{...eachbutton}
+                    (eachbutton.id === sum)?{...eachbutton, data: "rounded-full bg-green-700", isEqualToSum: true}:{...eachbutton}
                 )
                 // console.log(temp);
                 return temp;
             }    
         )
     },[randomNumber,sum])
+    
     function handleWin(s){
         if(s === 100){
             setIsWIn(true);
             //console.log("Win");
+            winclick();
         }
     }
-    
+
     function rollDice(){
+        playy();
         setFlag(true);
         console.log("f",f);
         if(f === false){
-            setButtonArray(
+            setButtonArray1(
                 (prev)=>{
                     temp = prev.map((eachbutton)=>
                         (eachbutton.id === sum)?{...eachbutton, data: "", isEqualToSum: false}:{...eachbutton}
@@ -78,7 +98,7 @@ function Game2(){
         }
         if(s <= 100){
             if(f === true){
-                setButtonArray(
+                setButtonArray1(
                     (prev)=>{
                         temp = prev.map((eachbutton)=>
                             (eachbutton.id === sum)?{...eachbutton, data: "", isEqualToSum: false}:{...eachbutton}
@@ -125,24 +145,31 @@ function Game2(){
     }
     
     return (
-        <div className="flex justify-center items-center h-[80vh]">
-        <div className="h-[485px] w-[485px] bg-[url('/ludoboard.jpg')] bg-no-repeat bg-green-500 bg-auto bg-center">
-            <div className="grid grid-cols-10 h-[485px] w-[485px]">
-            {arr.map((ele, index)=>{
-                return (<MyCell key={index} index={index} buttonArray={buttonArray}/>);
-            })}
+        <div className="flex justify-evenly items-center h-[80vh]">
+            <div className="h-[485px] w-[485px] bg-[url('/ludoboard.jpg')] bg-no-repeat bg-green-500 bg-auto bg-center">
+                <div className="grid grid-cols-10 h-[485px] w-[485px]">
+                {arr.map((ele, index)=>{
+                    return (<MyCell key={index} index={index} buttonArray1={buttonArray1}/>);
+                })}
+                </div>
             </div>
-            <div className="w-9 h-9 rounded-lg text-white font-bold text-2xl">{randomDiceImg}</div>
-            <button className="bg-yellow-300 text-pink-700 px-6 py-3 rounded mt-5" onClick={rollDice}>{(flag)?"Click to roll":"start"}</button>
-            <div className={`${(isWin)?"":"hidden"}`}>Win</div>    
+            <div className="flex flex-col justify-evenly items-center h-[485px] w-[485px] rounded-3xl border-4 border-yellow-300">
+                <div className="w-44 h-44 rounded-lg text-white font-bold text-2xl">{randomDiceImg}</div>
+                <button className="bg-yellow-300 text-pink-700 px-6 py-3 rounded mt-5" onClick={rollDice}>{(flag)?"Click to roll":"start"}</button>
+                <div className="font-mono font-bold text-center text-5xl text-cyan-200 pb-10">{(isWin)?"Win..!!":"Goo..."}</div> 
+            </div>
         </div>
-        </div>
-  );
-  function MyCell({index, buttonArray}){
-    return (
-            <div className={`${(buttonArray[index].isEqualToSum)?buttonArray[index].data:buttonArray[index].data}`}>{buttonArray[index].id}</div>
-        
     );
-  }
+
+    function MyCell({index, buttonArray1}){
+        return (
+            <div className="flex justify-center items-center">
+                <div className={`h-5 w-5 ${(buttonArray1[index].isEqualToSum)?buttonArray1[index].data:buttonArray1[index].data}`}>{buttonArray1[index].id}</div>
+                {/* <div className={`h-5 w-5 ${(buttonArray[index].isEqualToSum)?buttonArray[index].data:buttonArray[index].data}`}>{buttonArray[index].id}</div>
+                <div className={`h-5 w-5 ${(buttonArray[index].isEqualToSum)?buttonArray[index].data:buttonArray[index].data}`}>{buttonArray[index].id}</div>
+                <div className={`h-5 w-5 ${(buttonArray[index].isEqualToSum)?buttonArray[index].data:buttonArray[index].data}`}>{buttonArray[index].id}</div> */}
+            </div>
+        );
+    }
 }
 export default Game2;
