c51d4d69bad523e19c827f9c96b18e3e1826d1f8
pranti <prantibanda@gmail.com>
2024-02-21 01:02:40 +0530

    minor update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ function Navbar(){
     return(
         <>
             <div className="bg-orange-400 flex items-center py-3 border-b-8 border-yellow-300">
-                <div className="mx-10 font-bold text-3xl text-blue-950 italic">Play</div>
+                <div className="mx-10 font-bold text-3xl text-blue-950 italic">Joy-Junction</div>
                 <div className="flex space-x-10 ml-auto mr-10 text-white text-xl font-bold box-border">
                     <Link to="/"><div className="px-3 py-2 rounded-lg border-2 border-red-500 bg-orange-500 hover:border-white hover:bg-yellow-300 hover:text-pink-500 hover:cursor-pointer">Home</div></Link>
                     <Link to="/game1"><div className="px-3 py-2 rounded-lg border-2 border-red-500 bg-orange-500 hover:border-white hover:bg-yellow-300 hover:text-pink-500 hover:cursor-pointer">Game1</div></Link>
